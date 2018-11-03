805748d56d6d4897a0fd75ab446209dd32a30e7c
elena churilov <elena.churilov@gmail.com>
2018-11-03 13:28:52 -0700

    remove crud from webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -2,97 +2,37 @@ const path = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-// Is the current build a development build
-const IS_DEV = (process.env.NODE_ENV === 'dev');
-
-const dirNode = 'node_modules';
-const dirApp = path.join(__dirname, 'app');
-const dirAssets = path.join(__dirname, 'assets');
-
-const appHtmlTitle = 'Solovoi';
-
-/*
- * Webpack Configuration
- */
 module.exports = {
-    entry: {
-        vendor: [
-            'lodash'
-        ],
-        bundle: path.join(dirApp, 'index')
-    },
-    resolve: {
-        modules: [
-            dirNode,
-            dirApp,
-            dirAssets
-        ]
+    entry: "./app/index.js",
+    output: {
+        filename: "./solovoi.js"
     },
     plugins: [
-        new webpack.DefinePlugin({
-            IS_DEV: IS_DEV
-        }),
+        // new webpack.DefinePlugin({
+        //     IS_DEV: IS_DEV
+        // }),
 
         new HtmlWebpackPlugin({
             template: path.join(__dirname, 'index.ejs'),
-            title: appHtmlTitle
+            title: 'Solovoi'
         })
     ],
     module: {
         rules: [
-            // BABEL
             {
-                test: /\.js$/,
-                loader: 'babel-loader',
-                exclude: /(node_modules)/,
-                options: {
-                    compact: true
-                }
-            },
-
-            // STYLES
-            {
-                test: /\.css$/,
-                use: [
-                    'style-loader',
-                    {
-                        loader: 'css-loader',
-                        options: {
-                            sourceMap: IS_DEV
-                        }
-                    },
-                ]
-            },
-
-            // CSS / SASS
-            {
-                test: /\.scss/,
-                use: [
-                    'style-loader',
-                    {
-                        loader: 'css-loader',
-                        options: {
-                            sourceMap: IS_DEV
-                        }
-                    },
-                    {
-                        loader: 'sass-loader',
-                        options: {
-                            sourceMap: IS_DEV,
-                            includePaths: [dirAssets]
-                        }
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
                     }
-                ]
-            },
-
-            // IMAGES
-            {
-                test: /\.(jpe?g|png|gif)$/,
-                loader: 'file-loader',
-                options: {
-                    name: '[path][name].[ext]'
                 }
             }
         ]
+    },
+    devtool: "source-map",
+    resolve: {
+        extensions: [".js", ".jsx", "*"]
     }
 };
