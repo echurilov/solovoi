702ed7757eaa2b00d3d8fd372cde3aa962aa0aba
elena churilov <elena.churilov@gmail.com>
2018-11-03 18:31:25 -0700

    Optimize cell drawing

--- app/index.js
+++ app/index.js
@@ -2,29 +2,52 @@ import { Delaunay } from "d3-delaunay";
 
 document.addEventListener("DOMContentLoaded", () => {
   const canvasEl = document.getElementsByTagName("canvas")[0];
+  const ctx = canvasEl.getContext("2d");
+
   canvasEl.width = self.innerWidth;
   canvasEl.height = self.innerHeight;
 
-  let cellCount = canvasEl.width * canvasEl.height / 5000;
-  const points = [];
-  for (let i = 0; i < cellCount; i++) {
+  let points = [];
+
+  // // Set up border cells
+  // let borderDensity = 10
+
+  // points.push([-100, -100], [canvasEl.width + 100, canvasEl.height + 100])
+  // for (let w = 0; w < canvasEl.width; w += canvasEl.width / borderDensity) {
+  //   points.push([w, -100]);
+  //   points.push([w, canvasEl.height + 100]);
+  // }
+
+  // for (let h = 0; h < canvasEl.height; h += canvasEl.height / borderDensity) {
+  //   points.push([-100, h]);
+  //   points.push([canvasEl.width + 100, h]);
+  // }
+
+  let cellCount = points.length + (canvasEl.width * canvasEl.height / 5000);
+
+  for (let i = points.length; i < cellCount; i++) {
     points.push([
       Math.floor(canvasEl.width * Math.random()),
       Math.floor(canvasEl.height * Math.random())
     ]);
   }
-  
+
   const delaunay = Delaunay.from(points)
   const voronoi = delaunay.voronoi([0, 0, canvasEl.width, canvasEl.height]);
 
-  const ctx = canvasEl.getContext("2d");
-
   function clearCanvas() {
     voronoi.render(ctx);
-    ctx.fillStyle = "#6495ED"
+    ctx.fillStyle = "rgba(0, 128, 255, 1.0)"
     ctx.fillRect(0, 0, canvasEl.width, canvasEl.height);
-    ctx.strokeStyle = "#00FFFF"
+    ctx.strokeStyle = "rgba(0, 255, 255, 1.0)"
     ctx.lineWidth = 3;
+    ctx.stroke();
+  }
+
+  function drawDelaunay() {
+    delaunay.render(ctx);
+    ctx.strokeStyle = "rgba(0, 0, 0, 1.0)"
+    ctx.lineWidth = 1;
     ctx.stroke();
   }
 
@@ -44,15 +67,39 @@ document.addEventListener("DOMContentLoaded", () => {
     ctx.stroke(cell);
   }
 
+  function fillCells(cells, backgroundColor, borderColor) {
+    for (let cell of cells) {
+      fillCell(cell, backgroundColor, borderColor)
+    }
+  }
+
+  let prevHoverCell = 0;
+  let currentHoverCell = 0;
+  let currentNeighbors = [];
+  let prevNeighbors = [];
+  let otherCells = [];
+
   canvasEl.addEventListener('mousemove', function (e) {
     let mousePos = getMousePos(canvasEl, e);
-    for (let i = 0; i < cellCount; i++) {
-      if (voronoi.contains(i, mousePos.x, mousePos.y)) {
-        fillCell(i, '#00008B', '#00FFFF')
-        console.log(i, [...delaunay.neighbors(i)])
-      } else {
-        fillCell(i, '#6495ED', '#00FFFF')
-      };
+    if (!voronoi.contains(currentHoverCell, mousePos.x, mousePos.y)) {
+      for (let i = 0; i < cellCount; i++) {
+        if (voronoi.contains(i, mousePos.x, mousePos.y)) {
+          prevHoverCell = currentHoverCell;
+          currentHoverCell = i;
+        } else {
+          otherCells.push(i)
+        };
+      }
+    }
+    if (currentHoverCell != prevHoverCell) {
+      prevNeighbors = currentNeighbors;
+      currentNeighbors = [...delaunay.neighbors(currentHoverCell)];
+      // console.log("previous: ", prevHoverCell, prevNeighbors);
+      // console.log("current: ", currentHoverCell, currentNeighbors);
+      fillCells(prevNeighbors, "rgba(0, 128, 255, 1.0)", "rgba(0, 255, 255, 1.0)");
+      fillCell(prevHoverCell, "rgba(0, 128, 255, 1.0)", "rgba(0, 255, 255, 1.0)");
+      fillCells(currentNeighbors, "rgba(128, 128, 255, 1.0)", "rgba(0, 255, 255, 1.0)");
+      fillCell(currentHoverCell, "rgba(0, 0, 255, 1.0)", "rgba(255, 255, 255, 1.0)");
     }
   }, false);
 
